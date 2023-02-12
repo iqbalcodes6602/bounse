9dbc5585b2cf2cce025116a183db83dc9d91a2b1
noobcoder6602 <mohd.abd.6602@gmail.com>
2023-02-12 15:12:51 +0530

    env added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes')
-const {registerUser, authUser} = require("./controllers/userController")
+const {authUser} = require("./controllers/userController")
 const chatRoutes = require('./routes/chatRoutes')
 const messageRoutes = require('./routes/messageRoutes')
 const productRoutes = require('./routes/productRoutes')
 const {notFound, errorHandler} = require("./middleware/errorMiddleware")
 const cors = require("cors")
+
 
 dotenv.config();
 const app = express();
@@ -32,16 +32,5 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-const PORT = 5000;
-const server = app.listen(PORT, console.log("server started  on port 5000"));
-
-const io = require('socket.io')(server, {
-    pingTimeout: 60000,
-    cors:{
-        origin:"http://localhost:3000"
-    }
-})
-
-io.on('connection', (socket) => {
-    console.log("connected to socket.io")
-})
+const PORT = process.env.PORT ;
+app.listen(PORT, console.log(`server started  on port ${PORT}`));
