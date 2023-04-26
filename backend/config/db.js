fa4e1fbfd8137fad5cfc83888ef64bc22f0ae7bc
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-04-26 16:25:35 +0530

    mongodb URI added

--- backend/config/db.js
+++ backend/config/db.js
@@ -15,6 +15,7 @@ const connectDB = async()=>{
     // }catch(error){
     //     console.log(`Error: ${error.message}`);
     //     process.exit();
+    //
     // }
     try{
         const conn = await mongoose.connect("mongodb://localhost:27017/chatDB",{
