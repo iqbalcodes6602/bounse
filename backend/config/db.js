0bed496092be2b44c955dec09002912366a633f8
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-09-08 20:09:21 +0530

    mongo added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,28 +2,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const MONGODB_URI = process.env.MONGODB_URI ;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const connectDB = async()=>{
-    
-    // try{
-    //     const conn = await mongoose.connect(`${MONGODB_URI}`,{
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //     });
-    //     console.log(`MongoDB Conencted ${conn.connection.host}`);
-    // }catch(error){
-    //     console.log(`Error: ${error.message}`);
-    //     process.exit();
-    //
-    // }
-    try{
-        const conn = await mongoose.connect("mongodb://localhost:27017/chatDB",{
+const connectDB = async () => {
+
+    try {
+        // const conn = await mongoose.connect("mongodb://localhost:27017/chatDB",{
+        const conn = await mongoose.connect(`${MONGODB_URI}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log(`MongoDB Conencted ${conn.connection.host}`);
-    }catch(error){
+    } catch (error) {
         console.log(`Error: ${error.message}`);
         process.exit();
     }
